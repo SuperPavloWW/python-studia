aa06e4a0a23f42622d876eca5286021b80a065bd
SuperPavloWW <64170831+SuperPavloWW@users.noreply.github.com>
2020-12-07 16:47:40 +0100

    Update PodmienianieSlow.py
    regex approach

--- studia/PodmienianieSlow.py
+++ studia/PodmienianieSlow.py
@@ -11,28 +11,31 @@ Created on Sat Nov 21 18:28:22 2020
 
 @author: super
 """
+
+import re
+
 with open('text2.txt', 'r', encoding="utf8") as file:
     data = file.read()
     
-words = data.split()
+#words = data.split()
 replacable = {"i" : "oraz",
               "oraz" : "i",
               "nigdy" : "prawie nigdy",
-              "dlaczego" : "czemu"}
+              "dlaczego" : "czemu",
+              "literackiego" : "indyjskiego",
+              "ten" : "Ów"}
+
+
+words = re.split('(\W+)', data)
 
 newwords = []
 
 for word in words: 
-    if word in replacable:
+    if word in replacable or word.lower() in replacable:
         #print(replacable[word])
-        newwords.append(replacable[word])
+        newwords.append(replacable[word.lower()])
         continue
     newwords.append(word)
-
-result = ' '.join(newwords)
+    
+result = ''.join(newwords)
 print(result)
-
-#pisanie do nowego pliku
-#text_file = open("newfile2.txt", "w+")
-#n = text_file.write(result)
-#text_file.close()
